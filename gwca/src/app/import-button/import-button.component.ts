3171e421fbf242ba35e8547a63b4df609e831fc8
bBanker <fireninja171@gmail.com>
2019-09-12 14:03:53 -0400

    Created new buttons

---
+++ gwca/src/app/import-button/import-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-import-button',
+  templateUrl: './import-button.component.html',
+  styleUrls: ['./import-button.component.css']
+})
+export class ImportButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
