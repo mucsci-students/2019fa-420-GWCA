5819adee8cbf505c870b134d0db3d4fbdf5c0503
Joe Dunton (work machine) <duntonjoe@gmail.com>
2019-09-13 13:45:33 -0400

    added test function, needs mapped to a component

---
+++ gwca/src/app/app.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core'
+
+@Injectable({
+  providedIn: 'root',
+})
+
+
+test() {
+	console.log("success");
+}
+
+export class AppService {
+
+  constructor() { }
+
+}
