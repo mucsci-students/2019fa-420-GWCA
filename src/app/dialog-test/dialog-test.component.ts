ae67399dda0f0aaba2625a07c73f3a4512f9c542
bBanker <fireninja171@gmail.com>
2019-10-21 11:49:38 -0400

    Updated logic so that on edit, connections redraw
    I had forgottnen about this case, but now it works. It will have to wait
    until next sprint, since I won't break the hard deadline.

--- src/app/dialog-test/dialog-test.component.ts
+++ src/app/dialog-test/dialog-test.component.ts
@@ -50,7 +50,52 @@ export class DialogTestComponent implements OnInit {
     var cls = document.querySelector('.'+CSS.escape(this.className));
     
     //update
-    cls.remove();
+    //remove old endpoints
+    this.service.jsPlumbInstance.reset();
+
+    var class_boxes = document.querySelectorAll("app-class-box");
+
+        //re-initialize data
+        for(var i = 0;i<class_boxes.length;i++){
+           this.service.jsPlumbInstance.addEndpoint(class_boxes[i]['childNodes'][0]['id'],{anchor:"Top",uuid:(class_boxes[i]['firstChild']['attributes']['id'].value+"_top")},this.service.common);
+           this.service.jsPlumbInstance.addEndpoint(class_boxes[i]['childNodes'][0]['id'],{anchor:"Bottom",uuid:(class_boxes[i]['firstChild']['attributes']['id'].value+"_bottom")},this.service.common);
+           this.service.jsPlumbInstance.addEndpoint(class_boxes[i]['childNodes'][0]['id'],{anchor:"Right",uuid:(class_boxes[i]['firstChild']['attributes']['id'].value+"_right")},this.service.common);
+           this.service.jsPlumbInstance.addEndpoint(class_boxes[i]['childNodes'][0]['id'],{anchor:"Left",uuid:(class_boxes[i]['firstChild']['attributes']['id'].value+"_left")},this.service.common);
+          
+          // //re-bind the "no link to self rule"
+          var jsPlumbInstance = this.service.jsPlumbInstance;
+          this.service.jsPlumbInstance.bind("connection",function(endpoint){
+              if(endpoint['source']['attributes'][4].value == endpoint['target']['attributes'][4].value){
+                var connection = jsPlumbInstance.getConnections({source: endpoint['source']['attributes'][4].value,target: endpoint['target']['attributes'][4].value });
+                jsPlumbInstance.deleteConnection(connection[0]);
+              }
+           });
+
+        }
+
+
+    var classes = this.service.allClasses;
+    for(var i = 0;i<classes.length;i++){
+      if(classes[i]['connections'].length !== 0){
+        for(var j = 0;j<classes[i]['connections'].length;j++){
+          //have to format the uuid a little to get the update element
+         var source = classes[i]['connections'][j][0].split("_")[0];
+         var sourcePosition = classes[i]['connections'][j][0].split("_")[1];
+         var srcElement = document.querySelector("app-class-box ."+source).id;
+         var target = classes[i]['connections'][j][1].split("_")[0]
+         var targetPosition = classes[i]['connections'][j][1].split("_")[1];
+         var targetElement = document.querySelector("app-class-box ."+target).id;
+
+         var connectionType = classes[i]['connections'][j][2];
+         this.service.jsPlumbInstance.connect({
+           uuids:[(srcElement+"_"+sourcePosition),(targetElement+"_"+targetPosition)],
+           paintStyle: {stroke: connectionType, lineWidth: '10px'},
+         });
+        }
+
+     }
+    }
+    //cls.remove();
   }
 
   replaceUndefined(){
