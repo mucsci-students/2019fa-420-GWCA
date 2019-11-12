d0894df7339dfe3565e7592a7ffb3c4168c5371b
bBanker <fireninja171@gmail.com>
2019-11-12 16:35:24 -0500

    fixing coveralls 1

--- karma.conf.js
+++ karma.conf.js
@@ -17,7 +17,7 @@ module.exports = function (config) {
     },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, './coverage/gwca'),
-      reports: ['html', 'lcovonly', 'text-summary','lcov.info'],
+      reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
     customLaunchers: {
